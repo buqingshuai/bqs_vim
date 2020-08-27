00b3b0ba428487099c78f25a6b06915c12178d59
unknown <buqingshuai@longcheer.com>
2020-08-27 18:53:09 +0800

    ﻿bqs frist vim config

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Author: wanghan
+# Created Time : 2020年08月16日 12:14:35
+# File Name: install.sh
+# Description: install and reinstall dst_vimconfig
+
+
+echo "Installing vim ..."
+sudo apt install vim
+
+echo "Installing ctags cscope ..."
+sudo apt install ctags cscope
+
+read -N 1 -p "Do you want delete old config?(y/n):" answer
+echo ""
+if [ $answer == "Y" -o $answer == "y" ]; then
+	echo "Remove old vimcofnig ..."
+	echo "Delete ~/.vim ..."
+	rm -rf ~/.vim
+	echo "Delete ~/.vimrc ..."
+	rm -rf ~/.vimrc
+fi
+
+if [ -d ~/.vim -o -f ~/.vimrc ]; then
+	echo "Install Failed!"
+	echo "Please backup your ~/.vimrc and ~/.vim"
+	exit 1
+fi
+
+echo "Link dst_vimconfig/imrc to ~/.vimrc ..."
+ln -s ~/.bqs_vim/vimrc $HOME/.vimrc
+
+echo "Installing vim-plug to ~/.vim/autoload/plug.vim ..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+echo "Installing vim plugins ..."
+vim +PlugInstall
+
+echo "Done."
